b66e0d45d02262acf88a191e7908b8dcd54d631d
Wallas Maciel <wallasmaciel919@gmail.com>
2022-12-10 16:16:38 -0300

    - Configuração inicial do projeto de replica do Youtube.

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{ts,tsx}"],
+  theme: {
+    colors: { 
+      transparent: 'transparent',
+
+      state: {
+        '50': '#f8fafc',
+        '100': '#f1f5f9', 
+        '300': '#cbd5e1', 
+        '400': '#94a3b8', 
+        '500': '#64748b', 
+        '600': '#475569',
+        '700': '#334155',
+        '800': '#1e293b',
+        '900': '#0F172A'
+      },
+
+      cyan: {
+        500: '#81d8f7',
+        300: '#9BE1FB'
+      }
+    },
+    extend: {  
+      height: {
+        headerNavigationBar: '3.5rem',
+      },
+    }
+  },
+  plugins: [],
+}
